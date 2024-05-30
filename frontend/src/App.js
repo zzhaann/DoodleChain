9b6174ee8b1a05ca7ac450f8fe0830a833ddb68f
zzhaann <zhhaann@mail.ru>
2024-05-30 21:53:47 +0600

    adding websocket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import CanvasComponent from './CanvasComponent';
+import CanvasComponent from './components/CanvasComponent';
+
 
 const socket = io('http://localhost:3000');
 
